7c59d9fa4ea5aa566397f3df88b3719885577d8a
walynde-arch <walynde@gmail.com>
2026-07-16 17:22:24 +0200

    fork: remove PRO license check, always return pro tier

---
+++ dashboard/dist/assets/InfoLabel-BAhsgLcj.js
@@ -0,0 +1,1 @@
+import{j as r,k as e}from"./index-CnXGGM07.js";function s({label:t,tooltip:o}){return r.jsx(e,{text:o,children:t})}export{s as I};
---
+++ dashboard/dist/assets/TooltipText-CGH7dKN6.js
@@ -0,0 +1,1 @@
+import{j as r,k as s}from"./index-CnXGGM07.js";function p({text:o,children:t}){return r.jsx(s,{text:o,children:t})}export{p as T};
